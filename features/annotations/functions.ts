661e3e11106a08ad94f0cc489aed8a27184e84a1
Yusuf Eren <erenyusuf170@gmail.com>
2022-09-24 03:05:00 +0300

    Function types declarations

--- features/annotations/functions.ts
+++ features/annotations/functions.ts
@@ -2,3 +2,18 @@
 const add = (a: number, b: number): number => {
   return a - b;
 };
+// if we don't declare the return type
+// Typescript can't help us about type declaration
+const subtract = (a: number, b: number): number => {
+  return a - b;
+};
+
+function divide(a: number, b: number): number {
+  return a / b;
+}
+
+const multiply = function (a: number, b: number): number {
+  return a * b;
+};
+
+// ALL TYPES OF FUNCTIONS WITH DECLARATIONS
