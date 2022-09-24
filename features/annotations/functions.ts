0cf3c5d19bfce99947ee9ef2ea54fe1b355e19b5
Yusuf Eren <erenyusuf170@gmail.com>
2022-09-24 03:14:52 +0300

    Destructuring

--- features/annotations/functions.ts
+++ features/annotations/functions.ts
@@ -1,3 +1,5 @@
+// ALL TYPES OF FUNCTIONS WITH DECLARATIONS
+
 // defining parameter types and !!! type of the value to be returned
 const add = (a: number, b: number): number => {
   return a - b;
@@ -16,4 +18,25 @@ const multiply = function (a: number, b: number): number {
   return a * b;
 };
 
-// ALL TYPES OF FUNCTIONS WITH DECLARATIONS
+const logger = (message: string): void => {
+  console.log(message);
+};
+
+const throwError = (message: string): void => {
+  if (!message) {
+    throw new Error(message);
+  }
+};
+
+const forecast = {
+  date: new Date(),
+  weather: "sunny",
+};
+// if a function doesn't return any value
+// then you can specify void as return type
+// --- Destructuring ---
+const logWeather = ({ date, weather }: { date: Date; weather: string }): void => {
+  console.log(date, weather);
+};
+
+logWeather(forecast);
