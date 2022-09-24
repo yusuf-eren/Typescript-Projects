a5a1d9c6f5e91983d8eabbf3fb71ff33f17736d0
Yusuf Eren <erenyusuf170@gmail.com>
2022-09-24 03:45:29 +0300

    arrays

---
+++ features/arrays.ts
@@ -0,0 +1,6 @@
+const carMakers: string[] = ["ford", "toyota", "chevy"];
+const dates = [new Date(), new Date()];
+
+const carsByMake = [["f150"], ["corolla"], ["camaro"]];
+const x: string[][] = [];
+console.log(x);
