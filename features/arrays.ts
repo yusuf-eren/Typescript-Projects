37599864b96407f662c52398fb1b6c8daefce79a
Yusuf Eren <erenyusuf170@gmail.com>
2022-09-24 03:59:55 +0300

    arrays - i'm coming :)

--- features/arrays.ts
+++ features/arrays.ts
@@ -1,6 +1,22 @@
-const carMakers: string[] = ["ford", "toyota", "chevy"];
+const carMakers = ["ford", "toyota", "chevy"];
 const dates = [new Date(), new Date()];
 
 const carsByMake = [["f150"], ["corolla"], ["camaro"]];
 const x: string[][] = [];
-console.log(x);
+
+// Help with inference when extracting values
+const car = carMakers[0];
+const myCar = carMakers.pop();
+
+// Prevent incompatible values
+// carMakers.push(100);
+
+// Help with 'map'
+carMakers.map((car: string): string => {
+  return car.toUpperCase();
+});
+
+// Flexible types
+const importantDates: (Date | string)[] = [new Date(), "2030-10-10"];
+importantDates.push("2030-10-10");
+importantDates.push(new Date());
