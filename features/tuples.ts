cfb76e0c666c3b0f9dfb4b6a2ba03c353a6621c9
Yusuf Eren <erenyusuf170@gmail.com>
2022-09-24 04:37:34 +0300

    car

--- features/tuples.ts
+++ features/tuples.ts
@@ -9,3 +9,10 @@ type Drink = [string, boolean, number];
 const pepsi: Drink = ["brown", true, 40];
 const sprite: Drink = ["clear", true, 30];
 const tea: Drink = ["brown", false, 0];
+
+const carSpecs: [number, number] = [400, 3354];
+
+const carStats = {
+  horsepower: 400,
+  weight: 3354,
+};
