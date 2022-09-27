b9affb0928f2d5ee68ffd7495e5a1aecf2316fdb
yusuf-eren <erenyusuf170@gmail.com>
2022-09-27 03:34:10 +0300

    classes

--- features/classes.ts
+++ features/classes.ts
@@ -1,41 +1,3 @@
-// class Vehicle {
-//   color: string = "red";
-
-//   constructor(color: string) {
-//     this.color = color;
-//   }
-//   // Protected
-//   // This method can be called
-//   // By other methods in this class
-//   // OR by other methods in child classes
-//   protected honk(): void {
-//     console.log("beep");
-//   }
-// }
-
-// const vehicle = new Vehicle("orange");
-
-// // This class inherits all properties
-// // of 'Vehicle' class
-// class Car extends Vehicle {
-//   // Private
-//   // This method can only be called
-//   // By other methods in this class
-//   private drive(): void {
-//     console.log("vroom vroom");
-//     this.honk();
-//     console.log(this.color);
-//   }
-//   // Public
-//   // This method can be called any where, any time
-//   public startDrivingProcess(): void {
-//     this.drive();
-//   }
-// }
-
-// const car = new Car();
-// car.startDrivingProcess();
-
 class Vehicle {
   constructor(public color: string) {}
   // Protected
@@ -48,24 +10,33 @@ class Vehicle {
 }
 
 const vehicle = new Vehicle("orange");
-console.log(vehicle.color);
+//console.log(vehicle.color);
+
 // This class inherits all properties
 // of 'Vehicle' class
-//   class Car extends Vehicle {
-//     // Private
-//     // This method can only be called
-//     // By other methods in this class
-//     private drive(): void {
-//       console.log("vroom vroom");
-//       this.honk();
-//       console.log(this.color);
-//     }
-//     // Public
-//     // This method can be called any where, any time
-//     public startDrivingProcess(): void {
-//       this.drive();
-//     }
-//   }
+class Car extends Vehicle {
+  constructor(public wheels: number, public color: string) {
+    // that inherits color
+    // because we are not declaring again
+    // we just gave it in params and used
+    // by super method
+    super(color);
+  }
+  // Private
+  // This method can only be called
+  // By other methods in this class
+  private drive(): void {
+    console.log("vroom vroom");
+    this.honk();
+    console.log(this.color);
+  }
+  // Public
+  // This method can be called any where, any time
+  public startDrivingProcess(): void {
+    this.drive();
+  }
+}
 
-//   const car = new Car();
-//   car.startDrivingProcess();
+const car = new Car(4, "blue");
+car.startDrivingProcess(); // vroom vroom\n beep\n blue
+console.log(car); // Car { color: 'blue', wheels: 4 }
