9fd4b80bad7d6047ba598c22cde675cd92aacc88
yusuf-eren <erenyusuf170@gmail.com>
2022-09-27 03:25:50 +0300

    new  version

--- features/classes.ts
+++ features/classes.ts
@@ -1,4 +1,43 @@
+// class Vehicle {
+//   color: string = "red";
+
+//   constructor(color: string) {
+//     this.color = color;
+//   }
+//   // Protected
+//   // This method can be called
+//   // By other methods in this class
+//   // OR by other methods in child classes
+//   protected honk(): void {
+//     console.log("beep");
+//   }
+// }
+
+// const vehicle = new Vehicle("orange");
+
+// // This class inherits all properties
+// // of 'Vehicle' class
+// class Car extends Vehicle {
+//   // Private
+//   // This method can only be called
+//   // By other methods in this class
+//   private drive(): void {
+//     console.log("vroom vroom");
+//     this.honk();
+//     console.log(this.color);
+//   }
+//   // Public
+//   // This method can be called any where, any time
+//   public startDrivingProcess(): void {
+//     this.drive();
+//   }
+// }
+
+// const car = new Car();
+// car.startDrivingProcess();
+
 class Vehicle {
+  constructor(public color: string) {}
   // Protected
   // This method can be called
   // By other methods in this class
@@ -8,22 +47,25 @@ class Vehicle {
   }
 }
 
+const vehicle = new Vehicle("orange");
+console.log(vehicle.color);
 // This class inherits all properties
 // of 'Vehicle' class
-class Car extends Vehicle {
-  // Private
-  // This method can only be called
-  // By other methods in this class
-  private drive(): void {
-    console.log("vroom vroom");
-    this.honk();
-  }
-  // Public
-  // This method can be called any where, any time
-  public startDrivingProcess(): void {
-    this.drive();
-  }
-}
+//   class Car extends Vehicle {
+//     // Private
+//     // This method can only be called
+//     // By other methods in this class
+//     private drive(): void {
+//       console.log("vroom vroom");
+//       this.honk();
+//       console.log(this.color);
+//     }
+//     // Public
+//     // This method can be called any where, any time
+//     public startDrivingProcess(): void {
+//       this.drive();
+//     }
+//   }
 
-const car = new Car();
-car.startDrivingProcess();
+//   const car = new Car();
+//   car.startDrivingProcess();
