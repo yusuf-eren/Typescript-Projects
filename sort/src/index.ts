acc452a60d224e2eefa51068b80ae204b6a0f76c
yusuf-eren <erenyusuf170@gmail.com>
2022-09-30 02:26:53 +0300

    linkedList

--- sort/src/index.ts
+++ sort/src/index.ts
@@ -1,15 +1,25 @@
 import { Sorter } from "./Sorter";
 import { NumbersCollection } from "./NumbersCollection";
 import { CharactersCollection } from "./CharactersCollection";
+import { LinkedList } from "./LinkedList";
 
 const numbersCollection = new NumbersCollection([10, 3, -5, 0]);
 const characterCollection = new CharactersCollection("EGBaaSXZxyAa");
+const linkedList = new LinkedList();
+linkedList.add(500);
+linkedList.add(-10);
+linkedList.add(-25);
+linkedList.add(12);
+linkedList.add(1243);
 
 const numberSorter = new Sorter(numbersCollection);
 const characterSorter = new Sorter(characterCollection);
+const linkedListSorter = new Sorter(linkedList);
 
 numberSorter.sort();
 characterSorter.sort();
+linkedListSorter.sort();
 
 console.log(numbersCollection.data);
 console.log(characterCollection.data);
+console.log(linkedList.head);
