5f8667687abebed5545083d41cf8932d7bc498cf
yusuf-eren <erenyusuf170@gmail.com>
2022-09-30 02:50:29 +0300

    last fixes

--- sort/src/index.ts
+++ sort/src/index.ts
@@ -4,7 +4,7 @@ import { CharactersCollection } from "./CharactersCollection";
 import { LinkedList } from "./LinkedList";
 
 const numbersCollection = new NumbersCollection([10, 3, -5, 0]);
-const characterCollection = new CharactersCollection("EGBaaSXZxyAa");
+const charactersCollection = new CharactersCollection("EGBaaSXZxyAa");
 const linkedList = new LinkedList();
 linkedList.add(500);
 linkedList.add(-10);
@@ -12,14 +12,10 @@ linkedList.add(-25);
 linkedList.add(12);
 linkedList.add(1243);
 
-const numberSorter = new Sorter(numbersCollection);
-const characterSorter = new Sorter(characterCollection);
-const linkedListSorter = new Sorter(linkedList);
-
-numberSorter.sort();
-characterSorter.sort();
-linkedListSorter.sort();
+numbersCollection.sort();
+charactersCollection.sort();
+linkedList.sort();
 
 console.log(numbersCollection.data);
-console.log(characterCollection.data);
+console.log(charactersCollection.data);
 console.log(linkedList.head);
