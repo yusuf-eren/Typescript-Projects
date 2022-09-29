4e1321186ca92675878e81277c2e143b7a18589d
yusuf-eren <erenyusuf170@gmail.com>
2022-09-29 19:10:36 +0300

    new class integration

--- sort/build/index.js
+++ sort/build/index.js
@@ -2,7 +2,12 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const Sorter_1 = require("./Sorter");
 const NumbersCollection_1 = require("./NumbersCollection");
+const CharactersCollection_1 = require("./CharactersCollection");
 const numbersCollection = new NumbersCollection_1.NumbersCollection([10, 3, -5, 0]);
-const sorter = new Sorter_1.Sorter(numbersCollection);
-sorter.sort();
+const characterCollection = new CharactersCollection_1.CharactersCollection("EGBaaSXZxyAa");
+const numberSorter = new Sorter_1.Sorter(numbersCollection);
+const characterSorter = new Sorter_1.Sorter(characterCollection);
+numberSorter.sort();
+characterSorter.sort();
 console.log(numbersCollection.data);
+console.log(characterCollection.data);
--- sort/src/index.ts
+++ sort/src/index.ts
@@ -1,8 +1,15 @@
 import { Sorter } from "./Sorter";
 import { NumbersCollection } from "./NumbersCollection";
+import { CharactersCollection } from "./CharactersCollection";
 
 const numbersCollection = new NumbersCollection([10, 3, -5, 0]);
-const sorter = new Sorter(numbersCollection);
+const characterCollection = new CharactersCollection("EGBaaSXZxyAa");
 
-sorter.sort();
+const numberSorter = new Sorter(numbersCollection);
+const characterSorter = new Sorter(characterCollection);
+
+numberSorter.sort();
+characterSorter.sort();
+
 console.log(numbersCollection.data);
+console.log(characterCollection.data);
