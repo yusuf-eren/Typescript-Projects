eb63b69abf755bfe236346099d11b1eff6ca399f
yusuf-eren <erenyusuf170@gmail.com>
2022-09-30 02:50:22 +0300

    characters collection

--- sort/build/CharactersCollection.js
+++ sort/build/CharactersCollection.js
@@ -1,8 +1,10 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.CharactersCollection = void 0;
-class CharactersCollection {
+const Sorter_1 = require("./Sorter");
+class CharactersCollection extends Sorter_1.Sorter {
     constructor(data) {
+        super();
         this.data = data;
     }
     get length() {
