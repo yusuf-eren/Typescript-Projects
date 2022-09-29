8e4fca0eb621ad7cc0c5447d3d7287c87bf7528c
yusuf-eren <erenyusuf170@gmail.com>
2022-09-30 02:50:50 +0300

    abstraction on sorter

--- sort/src/Sorter.ts
+++ sort/src/Sorter.ts
@@ -4,16 +4,19 @@ interface Sortable {
   swap(leftIndex: number, RightIndex: number): void;
 }
 
-export class Sorter {
-  constructor(public collection: Sortable) {}
+export abstract class Sorter {
+  // abstract class declaration
+  abstract compare(leftIndex: number, RightIndex: number): boolean;
+  abstract swap(leftIndex: number, RightIndex: number): void;
+  abstract length: number;
 
   sort(): void {
-    const { length } = this.collection;
+    const { length } = this;
 
     for (let i = 0; i < length; i++) {
       for (let j = 0; j < length - i - 1; j++) {
-        if (this.collection.compare(j, j + 1)) {
-          this.collection.swap(j, j + 1);
+        if (this.compare(j, j + 1)) {
+          this.swap(j, j + 1);
         }
       }
     }
