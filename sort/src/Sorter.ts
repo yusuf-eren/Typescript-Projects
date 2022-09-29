a47f890109b9d6123074ea590771d4b89e648390
yusuf-eren <erenyusuf170@gmail.com>
2022-09-29 18:53:37 +0300

    final form o sorter

--- sort/src/Sorter.ts
+++ sort/src/Sorter.ts
@@ -1,7 +1,11 @@
-import { NumbersCollection } from "./NumbersCollection";
+interface Sortable {
+  length: number;
+  compare(leftIndex: number, RightIndex: number): boolean;
+  swap(leftIndex: number, RightIndex: number): void;
+}
 
 export class Sorter {
-  constructor(public collection: NumbersCollection) {}
+  constructor(public collection: Sortable) {}
 
   sort(): void {
     const { length } = this.collection;
