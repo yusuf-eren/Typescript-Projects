dbc93b145e9a21605df6305156c2d5587c5a9906
Yusuf Eren <erenyusuf170@gmail.com>
2022-10-01 03:27:58 +0300

    new function oop

---
+++ stats/build/CsvFileReader.js
@@ -0,0 +1,25 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.CsvFileReader = void 0;
+const fs_1 = __importDefault(require("fs"));
+class CsvFileReader {
+    // That fixes the unnecessary type identifies
+    constructor(filename) {
+        this.filename = filename;
+        this.data = [];
+    }
+    read() {
+        this.data = fs_1.default
+            .readFileSync(this.filename, {
+            encoding: "utf-8",
+        })
+            .split("\n")
+            .map((row) => {
+            return row.split(",");
+        });
+    }
+}
+exports.CsvFileReader = CsvFileReader;
---
+++ stats/src/CsvFileReader.ts
@@ -0,0 +1,19 @@
+import fs from "fs";
+
+export class CsvFileReader {
+  data: string[][] = [];
+
+  // That fixes the unnecessary type identifies
+  constructor(public filename: string) {}
+
+  read(): void {
+    this.data = fs
+      .readFileSync(this.filename, {
+        encoding: "utf-8",
+      })
+      .split("\n")
+      .map((row: string): string[] => {
+        return row.split(",");
+      });
+  }
+}
