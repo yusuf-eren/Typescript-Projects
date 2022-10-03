fdb96b2dcd57d89291940b6527bd9243d789c1f5
Yusuf Eren <erenyusuf170@gmail.com>
2022-10-03 16:49:57 +0300

    Generics final

--- features/generics.ts
+++ features/generics.ts
@@ -9,5 +9,39 @@ class ArrayOfAnything<TYPE> {
 const arrInt = new ArrayOfAnything<number>([1, 2, 4, 5]);
 const arrStr = new ArrayOfAnything<string>(["a", "5", "x", "ya"]);
 
-console.log(arrInt.get(2));
-console.log(arrStr.get(2));
+// example of generics with function
+function printAnything<TYPE>(arr: TYPE[]): void {
+  for (let i = 0; i < arr.length; i++) {
+    console.log(arr[i]);
+  }
+}
+
+// printAnything<number>([1, 5, 6, 7]);
+// printAnything<string>(["A", "C", "D", "X"]);
+
+// Generic Constraints
+
+class Car {
+  print() {
+    console.log("I am a car");
+  }
+}
+
+class House {
+  print() {
+    console.log("I am a house");
+  }
+}
+
+interface Printable {
+    print(): void
+}
+
+// inherits 'Printable' interface
+function printHousesOrCars<T extends Printable>(arr: T[]): void {
+  for (let i = 0; i < arr.length; i++) {
+    arr[i].print();
+  }
+}
+
+printHousesOrCars([new House(), new Car()])
